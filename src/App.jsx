914fd2c2369787f5ce56e98b36a0ae702383bd1d
Sardar balnour <balnour.sardar@gmail.com>
2026-05-09 02:21:22 +0330

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started mama sardar</h1>
+          <h1>Mama sardar react website</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
