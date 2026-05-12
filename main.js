8851b40e3502ce6ff8cc15cfdbe315dfef1d791e
Sardar balnour <balnour.sardar@gmail.com>
2026-05-12 16:15:29 +0330

    review

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("javascript review")
+// arrow function
+// map
+// filter and find method
+// reduce
+// template literals
+// destructuring
+// spred operator
+// ternary operator
---
+++ test.jsx
@@ -0,0 +1,8 @@
+
+function test() {
+  return (
+    <div>test</div>
+  )
+}
+
+export default test
