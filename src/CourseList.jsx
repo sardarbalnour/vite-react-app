5ab82930b033e5d19806d97eeb400a5d1fac95d0
Sardar balnour <balnour.sardar@gmail.com>
2026-05-20 16:16:57 +0330

    props

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,11 +1,18 @@
+import CourseCard from "./CourseCard";
+
 function CourseList() {
-  const courses = ["react", "js", "css", "html"];
+  const courses = [
+    { id: 1, name: "react", description: "this is react course" },
+    { id: 2, name: "js", description: "this is js course" },
+    { id: 3, name: "css", description: "this is css course" },
+    { id: 4, name: "next", description: "this is next course" },
+  ];
   return (
     <>
       <h3>CourseList</h3>
       <ul>
-        {courses.map((course, index) => (
-          <li key={index}>{course}</li>
+        {courses.map((course) => (
+          <CourseCard key={course.id} data={course}/>
         ))}
       </ul>
     </>
