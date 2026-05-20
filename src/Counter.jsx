333adfa879c0c2bfd9084575474cf63b9588356d
Sardar balnour <balnour.sardar@gmail.com>
2026-05-20 23:35:58 +0330

    state

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+// hook
+
 function Counter() {
+  const [counter, setCounter] = useState(0);
+
   const increaseHandler = () => {
-    console.log("first");
+    setCounter((counter) => counter + 1);
+  };
+
+  const stopHandler = () => {
+    setCounter("-");
   };
 
   return (
     <div>
-      <h1>Counter</h1>
+      <h1>{counter}</h1>
       <button onClick={increaseHandler}>Increase</button>
+      <button onClick={stopHandler}>stop</button>
     </div>
   );
 }
