2e864dbafa2991d0d59722ea419562d1b9cc6462
Sardar balnour <balnour.sardar@gmail.com>
2026-05-23 11:45:38 +0330

    submit

--- src/Form.jsx
+++ src/Form.jsx
@@ -9,7 +9,8 @@ function Form() {
     check: false,
   });
 
-  const loginHandler = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
     console.log(form);
   };
 
@@ -26,63 +27,65 @@ function Form() {
 
   return (
     <div>
-      <input
-        type="email"
-        name="email"
-        placeholder="Enter your email address : "
-        value={form.email}
-        onChange={changeHandler}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Enter password : "
-        value={form.password}
-        onChange={changeHandler}
-      />
-      <select value={form.role} name="role" onChange={changeHandler}>
-        <option value="admin">Admin</option>
-        <option value="user">User</option>
-        <option value="writer">Writer</option>
-      </select>
-      <div>
-        {/* name of all radiobuttons are equal in order to user just can select one option */}
-        <label htmlFor="male">Male</label>
-        {/* id should be equal to htmlfor in order to when we click on the label the radiobutton be selected */}
+      <form onSubmit={submitHandler}>
         <input
-          type="radio"
-          name="gender"
-          id="male"
-          value="male"
+          type="email"
+          name="email"
+          placeholder="Enter your email address : "
+          value={form.email}
           onChange={changeHandler}
-          checked={form.gender === "male"}
         />
-        <label htmlFor="female">Female</label>
         <input
-          type="radio"
-          name="gender"
-          id="female"
-          value="female"
+          type="password"
+          name="password"
+          placeholder="Enter password : "
+          value={form.password}
           onChange={changeHandler}
-          checked={form.gender === "female"}
         />
-        <label htmlFor="other">Other</label>
+        <select value={form.role} name="role" onChange={changeHandler}>
+          <option value="admin">Admin</option>
+          <option value="user">User</option>
+          <option value="writer">Writer</option>
+        </select>
+        <div>
+          {/* name of all radiobuttons are equal in order to user just can select one option */}
+          <label htmlFor="male">Male</label>
+          {/* id should be equal to htmlfor in order to when we click on the label the radiobutton be selected */}
+          <input
+            type="radio"
+            name="gender"
+            id="male"
+            value="male"
+            onChange={changeHandler}
+            checked={form.gender === "male"}
+          />
+          <label htmlFor="female">Female</label>
+          <input
+            type="radio"
+            name="gender"
+            id="female"
+            value="female"
+            onChange={changeHandler}
+            checked={form.gender === "female"}
+          />
+          <label htmlFor="other">Other</label>
+          <input
+            type="radio"
+            name="gender"
+            id="other"
+            value="other"
+            onChange={changeHandler}
+            checked={form.gender === "other"}
+          />
+        </div>
         <input
-          type="radio"
-          name="gender"
-          id="other"
-          value="other"
+          type="checkbox"
+          name="check"
+          checked={form.check}
           onChange={changeHandler}
-          checked={form.gender === "other"}
         />
-      </div>
-      <input
-        type="checkbox"
-        name="check"
-        checked={form.check}
-        onChange={changeHandler}
-      />
-      <button onClick={loginHandler}>Login</button>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
